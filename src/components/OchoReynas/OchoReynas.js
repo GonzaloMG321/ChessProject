a5fcdda05ef8a1927bd929460ccacc1023763104
Gonzalo <gonzalo.mendoza.itp@gmail.com>
2019-08-16 00:03:38 -0500

    Drag de las reynas al tablero

--- src/components/OchoReynas/OchoReynas.js
+++ src/components/OchoReynas/OchoReynas.js
@@ -2,31 +2,53 @@ import React, { useState } from 'react';
 import Tablero from './Tablero';
 
 function OchoReynas(){
-
-    const arrastrar = (ev) => {
-        //ev.preventDefault();
-        console.log("Arrastrando");
+    const REYNA_UNICODE = "\u2655";
+    const INITIAL_DRAGGABLE = true;
+    const [reynaUno, setReynaUno] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaDos, setReynaDos] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaTres, setReynaTres] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaCuatro, setReynaCuatro] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaCinco, setReynaCinco] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaSeis, setReynaSeis] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaSiete, setReynaSiete] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const [reynaOcho, setReynaOcho] = useState({isDraggable: INITIAL_DRAGGABLE, reynaUnicode: REYNA_UNICODE});
+    const setReynas = {
+        setReynaUno, setReynaDos, setReynaTres, setReynaCuatro, setReynaCinco, setReynaSeis, setReynaSiete, setReynaOcho
     }
-
-    const soltar = (ev) => {
-        //ev.preventDefault();
-        console.log("Soltando");
+    const arrastrar = (ev, reyna) => {
+        ev.dataTransfer.setData("setReyna", reyna);
     }
-
+    
     return(
         <div className="contenedor-ocho-reynas">
             <div className="tablero">
-                <Tablero></Tablero>
+                <Tablero setReynas={setReynas} ></Tablero>
             </div>
             <div className="seccion-piezas">
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
-                <div onDragStart={arrastrar} draggable="true" className="pieza-reyna">&#9813;</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaUno");
+                }} draggable={reynaUno.isDraggable} className={"pieza-reyna " + (reynaUno.isDraggable ? "cursor-pointer": "")} >{reynaUno.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaDos");
+                }} draggable={reynaDos.isDraggable} className={"pieza-reyna " + (reynaDos.isDraggable ? "cursor-pointer": "")}>{reynaDos.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaTres");
+                }} draggable={reynaTres.isDraggable} className={"pieza-reyna " + (reynaTres.isDraggable ? "cursor-pointer": "")}>{reynaTres.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaCuatro");
+                }} draggable={reynaCuatro.isDraggable} className={"pieza-reyna " + (reynaCuatro.isDraggable ? "cursor-pointer": "")}>{reynaCuatro.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaCinco");
+                }} draggable={reynaCinco.isDraggable} className={"pieza-reyna " + (reynaCinco.isDraggable ? "cursor-pointer": "")}>{reynaCinco.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaSeis");
+                }} draggable={reynaSeis.isDraggable} className={"pieza-reyna " + (reynaSeis.isDraggable ? "cursor-pointer": "")}>{reynaSeis.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaSiete");
+                }} draggable={reynaSiete.isDraggable} className={"pieza-reyna " + (reynaSiete.isDraggable ? "cursor-pointer": "")}>{reynaSiete.reynaUnicode}</div>
+                <div onDragStart={(e) => {
+                    arrastrar(e, "reynaOcho");
+                }} draggable={reynaOcho.isDraggable} className={"pieza-reyna " + (reynaOcho.isDraggable ? "cursor-pointer": "")}>{reynaOcho.reynaUnicode}</div>
             </div>
         </div>
     )
--- src/components/OchoReynas/Tablero.js
+++ src/components/OchoReynas/Tablero.js
@@ -1,29 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function Tablero(props){
-    const [ numeroReynas, setNumeroReynas ] = useState(0);
-    const tablero = [];
+function Tablero(props) {
+  const [numeroReynas, setNumeroReynas] = useState(0);
+  const [coordenadasReynas, setCoordenadasReynas] = useState([]);
+  const tablero = [];
 
-    const allowDrop = (evt) => {
-        console.log("Encimando....")
+  const allowDrop = ev => {
+    ev.preventDefault();
+  };
+
+  const drop = (ev, x, y) => {
+    ev.preventDefault();
+    if (numeroReynas < 8) {
+      setNumeroReynas(numeroReynas + 1);
+      let posicionReyna = {
+        numero: numeroReynas,
+        coordenada_x: x,
+        coordenada_y: y
+      };
+      let anteriores = coordenadasReynas;
+      anteriores.push(posicionReyna);
+      setCoordenadasReynas(anteriores);
+      let nuevo = { isDraggable: false, reynaUnicode: "" };
+      const reyna = ev.dataTransfer.getData("setReyna");
+      switch (reyna) {
+        case "reynaUno":
+          props.setReynas.setReynaUno(nuevo);
+          break;
+        case "reynaDos":
+          props.setReynas.setReynaDos(nuevo);
+          break;
+        case "reynaTres":
+          props.setReynas.setReynaTres(nuevo);
+          break;
+        case "reynaCuatro":
+          props.setReynas.setReynaCuatro(nuevo);
+          break;
+        case "reynaCinco":
+          props.setReynas.setReynaCinco(nuevo);
+          break;
+        case "reynaSeis":
+          props.setReynas.setReynaSeis(nuevo);
+          break;
+        case "reynaSiete":
+          props.setReynas.setReynaSiete(nuevo);
+          break;
+        case "reynaOcho":
+          props.setReynas.setReynaOcho(nuevo);
+          break;
+        default:
+          break;
+      }
     }
+  };
 
-    const drop = (evt) => {
-        console.log("Soltando...");
-    }
+  const checkIfQuenIsHere = (x, y) => {
+    let isHere = false;
+    coordenadasReynas.forEach(reyna => {
+      if (reyna.coordenada_x === x && reyna.coordenada_y === y) {
+        isHere = true;
+      }
+    });
+    return isHere;
+  };
 
-    for (var i = 7; i >= 0; i--) {
-        let fila = [];
-        for (var j = 0; j < 8; j++) {
-            fila.push(<div key={`${i}${j}`} onDragOver={allowDrop} onDrop={drop}  className={"casilla " + (i % 2 === 0 ? (j % 2 === 0  ? "negras " : "blancas "): (j % 2 !== 0 ? "negras ": "blancas "))}><div></div></div>)
-        }
-        tablero.push(<div key={`${i}`} className="fila">{fila}</div>);
+  for (var i = 7; i >= 0; i--) {
+    let fila = [];
+    for (var j = 0; j < 8; j++) {
+      let x = j + 1;
+      let y = i + 1;
+      let existeReyna = checkIfQuenIsHere(x, y);
+      fila.push(
+        <div
+          key={`${i}${j}`} onDragOver={allowDrop} onDrop={e => {
+            drop(e, x, y);
+          }}
+          className={
+            "casilla " + (i % 2 === 0 ? j % 2 === 0 ? "negras ": "blancas ": j % 2 !== 0 ? "negras ": "blancas ")
+          }>
+          <div draggable={existeReyna} className={existeReyna ? "casilla-ocupado": ""}>{existeReyna ? "\u2655" : ""}</div>
+        </div>
+      );
     }
-    return(
-        <React.Fragment>
-            {tablero}
-        </React.Fragment>
-    )
+    tablero.push(
+      <div key={`${i}`} className="fila">
+        {fila}
+      </div>
+    );
+  }
+  return <React.Fragment>{tablero}</React.Fragment>;
 }
 
 export default Tablero;
